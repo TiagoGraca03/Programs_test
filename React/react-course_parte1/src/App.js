8f372dff21d78a97c812300cb0059bae1fa1fc69
TiagoGraca03 <115741922+TiagoGraca03@users.noreply.github.com>
2023-02-13 21:19:57 +0000

    react commit

---
+++ React/react-course_parte1/src/App.js
@@ -0,0 +1,17 @@
+import Todo from "./components/Todo";
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>My Todos !!</h1>
+      <Todo text="React 1"/>
+      <Todo text="React 2"/>
+      <Todo text="React 3"/>
+      
+    </div>
+  );
+}
+
+export default App;
